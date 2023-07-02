ccd93646fa18c78d9b791955c5a6171ac35a75f3
Lucien Lasseau <62194841+AlurienFlame@users.noreply.github.com>
2023-07-02 17:02:34 +1200

    Checkboxes for module selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,38 @@
 <script lang="ts">
   let modules: string[];
-  let pkg: Blob;
+  let selected: Record<string, boolean> = {};
+  $: selectedModules = Object.keys(selected).filter((k) => selected[k]);
 
   import { onMount } from "svelte";
 
   onMount(async () => {
     let response = await fetch("/api/modules");
     modules = await response.json();
-    response = await fetch("/api/package", { method: "POST", body: JSON.stringify(modules) });
-    pkg = await response.blob();
   });
+
+  let pkg: Blob;
+  async function createPackage() {
+    let response = await fetch("/api/package", { method: "POST", body: JSON.stringify(modules) });
+    pkg = await response.blob();
+  }
 </script>
 
 <svelte:head>
   <title>Flame Tweaks</title>
 </svelte:head>
+
 <h1>Modules</h1>
-{#if modules !== undefined}
-  <p>Modules: {modules.join(", ")}</p>
-{/if}
+
+<!-- Module Selection -->
+<form>
+  {#each modules || [] as m}
+    <label for={m}>{m}</label>
+    <input type="checkbox" id={m} value={m} bind:checked={selected[m]} />
+  {/each}
+</form>
+
+<!-- Download Package -->
+<button on:click={createPackage}>Create Package</button>
 {#if pkg !== undefined}
   <a href={URL.createObjectURL(pkg)} download="package.zip">Download</a>
 {/if}
