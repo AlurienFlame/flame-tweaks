9ea7ba600f93a98b82fece84bc893fc3537fa53b
Lucien Lasseau <62194841+AlurienFlame@users.noreply.github.com>
2023-07-02 17:26:20 +1200

    Use relative file paths

--- src/routes/api/package/+server.ts
+++ src/routes/api/package/+server.ts
@@ -7,12 +7,12 @@ export async function POST({ request }: { request: Request; }) {
   let modules = await request.json();
 
   // Create a new folder for the package
-  let packageFolder = `/home/lucien/dev/flame-tweaks/static/packages/${Date.now()}`;
-  fse.copySync(`/home/lucien/dev/flame-tweaks/static/packages/template`, packageFolder);
+  let packageFolder = `./static/packages/${Date.now()}`;
+  fse.copySync(`./static/packages/template`, packageFolder);
 
   // Copy the modules into the package folder
   for (let module of modules) {
-    fse.copySync(`/home/lucien/dev/flame-tweaks/static/modules/${module}`, `${packageFolder}/${module}`);
+    fse.copySync(`./static/modules/${module}`, `${packageFolder}/${module}`);
   }
 
   // Zip the package folder
